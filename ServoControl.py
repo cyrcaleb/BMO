30ac9f04c12663628c416f8306ecfd44f3798d77
Caleb Cyr <90429575+cyrcaleb@users.noreply.github.com>
2022-09-02 15:24:01 -0400

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -2,54 +2,54 @@ from time import *
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-kit.servo[0].angle = 0
-kit.servo[1].angle = 0
+kit.servo[0].angle = 100
+kit.servo[1].angle = 130
 
 sleep(10)
 
 while True:
-  kit.servo[0].angle = 90
-  kit.servo[1].angle = 90
+  kit.servo[0].angle = 180
+  kit.servo[1].angle = 30
   
   sleep(3)
   
-  kit.servo[0].angle = 0
-  kit.servo[1].angle = 0
+  kit.servo[0].angle = 100
+  kit.servo[1].angle = 130
   
   sleep(3)
   
-  kit.servo[0].angle = 90
+  kit.servo[0].angle = 180
   
   sleep(1)
   
-  kit.servo[1].angle = 90
+  kit.servo[1].angle = 30
   
   sleep(1)
   
-   kit.servo[0].angle = 0
+   kit.servo[1].angle = 130
   
   sleep(1)
   
-  kit.servo[1].angle = 0
+  kit.servo[0].angle = 100
   
   sleep(5)
   
-  kit.servo[0].angle = 90
-  kit.servo[1].angle = 0
+  kit.servo[0].angle = 100
+  kit.servo[1].angle = 30
   
   sleep(1)
   
-  kit.servo[0].angle = 0
-  kit.servo[1].angle = 90
+  kit.servo[0].angle = 180
+  kit.servo[1].angle = 130
   
   sleep(1)
   
-  kit.servo[0].angle = 90
-  kit.servo[1].angle = 0
+  kit.servo[0].angle = 100
+  kit.servo[1].angle = 30
   
   sleep(3)
   
-  kit.servo[0].angle = 0
+  kit.servo[0].angle = 130
   
   sleep(5)
     
